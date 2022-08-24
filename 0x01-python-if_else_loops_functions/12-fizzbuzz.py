42128ee132ac0224a550ed82a5c949bee77ed467
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-08-24 17:56:42 +0100

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,12 @@ def fizzbuzz():
  for I in range(101):
    if (I % 3 == 0 and I % 5 == 0):
       print("FizzBuzz", end = ' ')
+      continue
    elif (I % 3 == 0):
       print("Fizz", end = ' ')
+      continue
    elif (I % 5 == 0):
       print("Buzz", end = ' ')
+      continue
    else:
       print(I)
