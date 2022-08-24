853e7faf3441bae0cfcb4a727a624dfddaa9eb1b
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-08-24 14:43:52 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for I in range(0, 90):
- if (I = 10|20|30|40|50|60|70|80|90):
+ if (I = 10 or 20 or 30 or 40 or 50 or 60 or 70 or 80 or 90):
    continue
  else:
   print("{:02d}".format(I), end = ', ')
