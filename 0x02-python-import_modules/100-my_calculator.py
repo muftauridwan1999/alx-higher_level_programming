c81d39f2560780dbf6c723666397070af35bff77
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-08-25 19:41:35 +0100

    Create 100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__=="__main__"
+    import sys
+    from calculator_1 import add, sub, mul, div
+    argv = sys.argv[:1]
+    argv_count = len(argv)
+    operators = ["+", "-", "*", "/"]
+    if argv_count is not 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif argv_count[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] = "+":
+            print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
+        elif sys.argv[2] = "-":
+            print("{0:d} - {1:d} = {2:d}".format(a, b, sub(a, b)))
+        elif sys.argv[2] = "*":
+            print("{0:d} * {1:d} = {2:d}".format(a, b, mul(a, b)))
+        elif sys.argv[2] = "/":
+            print("{0:d} / {1:d} = {2:d}".format(a, b, div(a, b)))
+     
