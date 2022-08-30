cc3b104e38f9e5523308939b17cc589b299339d5
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-08-30 16:55:58 +0100

    Create 10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if not my_list:
+        return my_list
+    for I in newlist:
+        if I % 2 == 0:
+            newlist[I] = True
+        else:
+            newlist[I] = False
+    return newlist
