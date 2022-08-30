15804e4571dd2c1f21dcca67918bc09bd11ebeba
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-08-30 11:13:51 -0700

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    if idx >= my_list:
+    elif idx >= my_list:
         return my_list
     my_list[idx] = element
     return my_list
