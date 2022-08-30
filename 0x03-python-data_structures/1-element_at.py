67ed632575333ac4a7c7e37fc398323c72b1ccd2
muftauridwan1999 <105045499+muftauridwan1999@users.noreply.github.com>
2022-08-30 16:13:46 +0100

    Create 1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+  for idx in my_list
+     if idx < 0:
+         return None
+     
+     if idx >= len(my_list)
+         return None
+     return my_list[idx]
+  
